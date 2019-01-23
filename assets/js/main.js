610a76699d8e56e45007195fd9a37ee8d1f84d37
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-23 11:11:05 +0100

    put everything into a function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,27 @@
 const food = ["cow", "potato", "chicken", "corn"];
-cookedFood = food.map(item => {
-  switch (item) {
-    case "cow":
-      return "hamburger";
-      break;
-    case "potato":
-      return "fries";
-      break;
-    case "chicken":
-      return "poultry_leg";
-      break;
-    case "corn":
-      return "popcorn";
-      break;
-  }
-});
-VegetarianCookedFood = cookedFood.filter(
-  food => food === "fries" || food === "popcorn"
-);
-ReducedFood = VegetarianCookedFood.reduce(
-  (accumulator, currentValue) => "shit"
-);
-console.log(ReducedFood);
+function ArrayMapFilterReduce(array) {
+  cookedFood = array.map(item => {
+    switch (item) {
+      case "cow":
+        return "hamburger";
+        break;
+      case "potato":
+        return "fries";
+        break;
+      case "chicken":
+        return "poultry_leg";
+        break;
+      case "corn":
+        return "popcorn";
+        break;
+    }
+  });
+  VegetarianCookedFood = cookedFood.filter(
+    food => food === "fries" || food === "popcorn"
+  );
+  ReducedFood = VegetarianCookedFood.reduce(
+    (accumulator, currentValue) => "shit"
+  );
+  return ReducedFood;
+}
+console.log(ArrayMapFilterReduce(food));
