1d46c0db1c1be75bedc65551cf52b0992f600f41
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-23 13:23:30 +0100

    wrote it to the DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,24 @@
 const food = ["cow", "potato", "chicken", "corn"];
-function ArrayMapFilterReduce(array) {
-  cookedFood = array.map(item => {
-    switch (item) {
-      case "cow":
-        return "hamburger";
-        break;
-      case "potato":
-        return "fries";
-        break;
-      case "chicken":
-        return "poultry_leg";
-        break;
-      case "corn":
-        return "popcorn";
-        break;
-    }
-  });
-  VegetarianCookedFood = cookedFood.filter(
-    food => food === "fries" || food === "popcorn"
-  );
-  ReducedFood = cookedFood.reduce((accumulator, currentValue) => "shit");
-  return ReducedFood;
-}
-console.log(ArrayMapFilterReduce(food));
+const cookedFood = food.map(item => {
+  switch (item) {
+    case "cow":
+      return "hamburger";
+      break;
+    case "potato":
+      return "fries";
+      break;
+    case "chicken":
+      return "poultry_leg";
+      break;
+    case "corn":
+      return "popcorn";
+      break;
+  }
+});
+const VegetarianCookedFood = cookedFood.filter(
+  food => food === "fries" || food === "popcorn"
+);
+const ReducedFood = cookedFood.reduce((accumulator, currentValue) => "shit");
+document.getElementById(
+  "root"
+).innerHTML = `<strong>Food source:</strong> ${food}<br><strong>prepared Food:</strong> ${cookedFood}<br><strong>Vegetarian food:</strong> ${VegetarianCookedFood}<br><strong>Final result after digestion:</strong>${ReducedFood}`;
